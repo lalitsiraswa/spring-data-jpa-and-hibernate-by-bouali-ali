f728d9d249ea1238404bab1900cfcd91c9935b80
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-12 07:47:14 +0530

    Code CleanUp & Using @Column in @Entity

--- src/main/java/com/learn/spring_boot_jpa/models/Author.java
+++ src/main/java/com/learn/spring_boot_jpa/models/Author.java
@@ -3,6 +3,8 @@ package com.learn.spring_boot_jpa.models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -10,28 +12,44 @@ import lombok.*;
 public class Author {
     // Primary-Key
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.TABLE,
-            generator = "author_id_generator"
-    )
+    @GeneratedValue
+//    (
+//           strategy = GenerationType.TABLE,
+//           generator = "author_id_generator"
+//    )
+
 //    @SequenceGenerator(
 //            name = "author_sequence",
 //            sequenceName = "author_sequence",
 //            allocationSize = 1
 //    )
-    @TableGenerator(
-            name = "author_id_generator",
-            table = "id_generator",
-            pkColumnName = "id_name",
-            valueColumnName = "id_value",
-            allocationSize = 1
-    )
+
+//    @TableGenerator(
+//            name = "author_id_generator",
+//            table = "id_generator",
+//            pkColumnName = "id_name",
+//            valueColumnName = "id_value",
+//            allocationSize = 1
+//    )
     private Integer id;
+    @Column(
+            length = 35
+    )
     private String firstName;
     private String lastName;
     @Column(
-            unique = true
+            unique = true,
+            nullable = false
     )
     private String email;
     private int age;
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModifies;
 }
