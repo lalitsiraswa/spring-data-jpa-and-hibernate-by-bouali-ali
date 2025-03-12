9b5e4fac282f2f89a171d0a71a1a201deddb0de0
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-12 21:50:30 +0530

    @Repository

--- src/main/java/com/learn/spring_boot_jpa/repositories/AuthorRepository.java
+++ src/main/java/com/learn/spring_boot_jpa/repositories/AuthorRepository.java
@@ -2,6 +2,8 @@ package com.learn.spring_boot_jpa.repositories;
 
 import com.learn.spring_boot_jpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository // Optional
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 }
