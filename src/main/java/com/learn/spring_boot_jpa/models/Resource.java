b80898fb0d80d4510d2aa51f5e6dda4d5f7fe0e8
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-13 23:17:04 +0530

    @OneToOne Relationship Between Lecture And Resource @Entity 'Bi-Directional'

--- src/main/java/com/learn/spring_boot_jpa/models/Resource.java
+++ src/main/java/com/learn/spring_boot_jpa/models/Resource.java
@@ -1,8 +1,6 @@
 package com.learn.spring_boot_jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,4 +18,9 @@ public class Resource {
     private String name;
     private int size;
     private String url;
+    @OneToOne
+    @JoinColumn(
+            name = "lecture_id"
+    )
+    private Lecture lecture;
 }
