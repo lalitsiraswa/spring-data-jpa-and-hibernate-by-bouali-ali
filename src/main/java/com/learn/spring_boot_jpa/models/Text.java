e3ecca633782008d65ac33993fb9e342041f99f1
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-14 13:22:45 +0530

    Created Text Class

---
+++ src/main/java/com/learn/spring_boot_jpa/models/Text.java
@@ -0,0 +1,16 @@
+package com.learn.spring_boot_jpa.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Text extends BaseEntity {
+    private String content;
+}
