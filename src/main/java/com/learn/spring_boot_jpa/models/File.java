2c250a481555ab8ae069e078dfb5c51860e87ee5
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-14 22:21:00 +0530

    @Inheritance(strategy = InheritanceType.SINGLE_TABLE) JPA - Single Table Strategy

--- src/main/java/com/learn/spring_boot_jpa/models/File.java
+++ src/main/java/com/learn/spring_boot_jpa/models/File.java
@@ -1,14 +1,18 @@
 package com.learn.spring_boot_jpa.models;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class File {
+@Entity
+public class File extends Resource{
     private String type;
 }
--- src/main/java/com/learn/spring_boot_jpa/models/Video.java
+++ src/main/java/com/learn/spring_boot_jpa/models/Video.java
@@ -1,14 +1,18 @@
 package com.learn.spring_boot_jpa.models;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class Video {
+@Entity
+public class Video extends Resource{
     private int length;
 }
