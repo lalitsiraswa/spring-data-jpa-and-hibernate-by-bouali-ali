50f4a6e549e5c58a5d2691cd0de3eb40e31819b9
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-14 07:26:59 +0530

    CleanUp

--- src/main/java/com/learn/spring_boot_jpa/SpringBootJpaApplication.java
+++ src/main/java/com/learn/spring_boot_jpa/SpringBootJpaApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication
 public class SpringBootJpaApplication {
 
