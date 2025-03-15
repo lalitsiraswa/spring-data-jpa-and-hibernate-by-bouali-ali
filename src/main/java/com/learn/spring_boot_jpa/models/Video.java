e9ac00e48051972b64cc7182cbf711c9a40f0cb8
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-15 11:28:42 +0530

    @Polymorphism(type = PolymorphismType.EXPLICIT)

--- src/main/java/com/learn/spring_boot_jpa/models/Video.java
+++ src/main/java/com/learn/spring_boot_jpa/models/Video.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Polymorphism;
+import org.hibernate.annotations.PolymorphismType;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -15,6 +17,7 @@ import lombok.experimental.SuperBuilder;
 @Entity
 // @PrimaryKeyJoinColumn(name = "video_id") -> It only works with the @Inheritance(strategy = InheritanceType.JOINED)
 // @DiscriminatorValue(value = "V")
+@Polymorphism(type = PolymorphismType.EXPLICIT)
 public class Video extends Resource {
     private int length;
 }
