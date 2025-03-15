d1a36e1d5fa0728ce4216e5535e361868134d0d3
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-15 15:02:22 +0530

    'AuthorRepository' - findAllByFirstName

--- src/main/java/com/learn/spring_boot_jpa/repositories/AuthorRepository.java
+++ src/main/java/com/learn/spring_boot_jpa/repositories/AuthorRepository.java
@@ -4,6 +4,9 @@ import com.learn.spring_boot_jpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository // Optional
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    List<Author> findAllByFirstName(String firstName);
 }
